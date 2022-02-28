786354e3a950355e0d623190f04e345f5f84eb66
K Vijith <vijith.messi@gmail.com>
2022-02-28 15:51:58 +0530

    fixed encoding again

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,12 +10,6 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Techo</title>
-        <style>
-          <style>
-            @import
-            url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&amp;display=swap");
-          </style>
-        </style>
         <meta name='description' content="Technovert's Bi-Monthly Magazine" />
       </Head>
       <Hero />
