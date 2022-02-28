1995832b228990507cd05d13ee22705d5828f933
K Vijith <vijith.messi@gmail.com>
2022-02-28 11:07:56 +0530

    made everything responsive🔥

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
 import Link from 'next/link'
 import styles from '../styles/Hero.module.css'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 const Navbar = () => {
   return (
     <div className={styles.navbarItems}>
       <div>
         <Link href='/'>
-            <img className={styles.logo} src='/logo.png' alt='Techo Logo' />
+          <img className={styles.logo} src='/logo.png' alt='Techo Logo' />
         </Link>
       </div>
       <div className={styles.navbarLinks}>
@@ -22,11 +23,31 @@ const Navbar = () => {
         </Link>
       </div>
       <div>
-      <button className='btn btn-dark'>
-        <Link href='#'>
-          <a>Subscribe</a>
-        </Link>
-      </button>
+        <div className={styles.navbarButton}>
+          <Link href='#'>
+            <a>Subscribe</a>
+          </Link>
+        </div>
+      </div>
+      {/* hamburger menu */}
+      <div className={styles.hamburgerMenu}>
+        <i className='bi bi-list'></i>
+        <div className={styles.hamburgerMenuItems}>
+          <Link href='#'>
+            <a>Home</a>
+          </Link>
+          <Link href='#'>
+            <a>About</a>
+          </Link>
+          <Link href='#'>
+            <a>Contact</a>
+          </Link>
+        </div>
+        <div className={styles.hamburgerMenuButton}>
+          <Link href='#'>
+            <a>Subscribe</a>
+          </Link>
+        </div>
       </div>
     </div>
   )
