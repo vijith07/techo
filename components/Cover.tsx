438abe007194023b242d7c262c3ae34a1df95add
K Vijith <vijith.messi@gmail.com>
2022-03-01 16:47:04 +0530

    vercel error fix #1

--- components/Cover.tsx
+++ components/Cover.tsx
@@ -1,40 +1,41 @@
 import styles from '../styles/Cover.module.css'
-import "bootstrap-icons/font/bootstrap-icons.css";
-import { useState } from "react";
-import dynamic from "next/dynamic";
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { useState } from 'react'
+import dynamic from 'next/dynamic'
 
-const PDFViewer = dynamic(() => import("../components/Flipbook"), {
-  ssr: false
-});
-
+const PDFViewer = dynamic(() => import('../components/Flipbook'), {
+  ssr: false,
+})
 
 interface CoverProps {
-    pathString: string
+  pathString: string
 }
 
+const Cover: React.FC<CoverProps> = (props) => {
+  const handleShow = (show: boolean) => {
+    setShowModal(true)
+  }
+  const [showModal, setShowModal] = useState(false)
+  function handleOutsideClick() {
+    //reload the window
+    window.location.reload()
+  }
 
-const Cover:React.FC<CoverProps> = (props) => {
-    const handleShow = (show: boolean) => {
-        setShowModal(true);
-    }
-    const [showModal, setShowModal] = useState(false);
-    function handleOutsideClick(event) {
-        //reload the window
-        window.location.reload();
-    
-      }
-    
   return (
     <div className={styles.coverContainer} onClick={handleShow}>
-        <PDFViewer show={showModal}  onClose={handleOutsideClick} />
-        <div className={styles.coverBody}>
-            <img src={props.pathString} alt="cover of the magazine issue" className={styles.coverImage}/>
-            <div className={styles.overlayContainer}>
-                <a href="#" className={styles.iconContainerLink} title="View">
-                    <i className="bi bi-eye-fill"></i>
-                </a>
-            </div>
+      <PDFViewer show={showModal} onClose={handleOutsideClick} />
+      <div className={styles.coverBody}>
+        <img
+          src={props.pathString}
+          alt='cover of the magazine issue'
+          className={styles.coverImage}
+        />
+        <div className={styles.overlayContainer}>
+          <a href='#' className={styles.iconContainerLink} title='View'>
+            <i className='bi bi-eye-fill'></i>
+          </a>
         </div>
+      </div>
     </div>
   )
 }
